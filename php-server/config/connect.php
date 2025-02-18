deecee6797037c818d35fe85c91ea5b6834b0133
DeLightPlus <matlakalakabelo1@gmail.com>
2025-02-18 15:07:33 +0200

    init react-app

--- php-server/config/connect.php
+++ php-server/config/connect.php
@@ -8,8 +8,5 @@
     $con = mysqli_connect($servername, $username, $password, $dbname);
 
     // Check connection
-    if (!$con) 
-    {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+    if (!$con) { die("Connection failed: " . mysqli_connect_error());  }
 ?>
